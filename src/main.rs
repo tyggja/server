13d895ebac21503ffa2995ffa00cb71412809c91
tyggja <tyggja@proton.me>
2022-10-31 16:09:00 -0400

    http buffer

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,21 @@
-use std::net::{TcpListener, TcpStream};
+use std::io::prelude::*;
+use std::net::TcpListener;
+use std::net::TcpStream;
 
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:80");
-    match listener {
-        Ok(val) => {
-            for stream in val.incoming() {
-                match stream {
-                    Ok(stream) => {
-                        // do something with the stream
-                        let mut data: [u8; 0];
-                        println!("[Client connected]: {:?}", stream.peer_addr());
-                    }
-                    Err(e) => {
-                        // print the error
-                        println!("{}", e.to_string());
-                    }
-                }
-            }
-        }
-        Err(e) => {
-            println!("Error: {}", e.to_string());
-        }
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        handle_connection(stream);
     }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+    //println!("[Client connected]: {:?}", stream.peer_addr());
+    // read data if there is any sent from the client
+    stream.read(&mut buffer).unwrap();
+    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
 }
