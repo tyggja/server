308eb94f8f9d9419d12985614710a98cde8836f8
tyggja <tyggja@proton.me>
2022-10-31 13:56:04 -0400

    bare_server_implementation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use std::net::{TcpListener, TcpStream};
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:80");
+    match listener {
+        Ok(val) => {
+            for stream in val.incoming() {
+                match stream {
+                    Ok(stream) => {
+                        // do something with the stream
+                        let mut data: [u8; 0];
+                        println!("[Client connected]: {:?}", stream.peer_addr());
+                    }
+                    Err(e) => {
+                        // print the error
+                        println!("{}", e.to_string());
+                    }
+                }
+            }
+        }
+        Err(e) => {
+            println!("Error: {}", e.to_string());
+        }
+    }
 }
