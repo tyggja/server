2c9473d60c2308dbbee39b2acc5d4086cd875d29
tyggja <tyggja@proton.me>
2022-10-31 16:24:33 -0400

    send response to the browser

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::fs;
 use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
@@ -8,14 +9,31 @@ fn main() {
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
+        // show client connection
+        println!("[Client connected]: {:?}", stream.peer_addr());
+
+        // handle each connection
         handle_connection(stream);
     }
 }
 
 fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
-    //println!("[Client connected]: {:?}", stream.peer_addr());
+
     // read data if there is any sent from the client
     stream.read(&mut buffer).unwrap();
+
+    let contents = fs::read_to_string("hello.html").unwrap();
+
     println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+
+    // write a status code back to the client
+    let response = format!(
+        "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
+        contents.len(),
+        contents
+    );
+
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
